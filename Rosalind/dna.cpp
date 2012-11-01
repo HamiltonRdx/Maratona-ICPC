697cc18e0e40b666868b992821d08787824fb215
Julio Silva <julio@juliobs.com>
2012-10-31 22:28:27 -0200

    Adicionei problemas do Rosalind

---
+++ Rosalind/dna.cpp
@@ -0,0 +1,39 @@
+// ============================================================================
+// 
+//       Filename:  dna.cpp
+// 
+//    Description:  Counting Nucleotides
+//                  rosalind.info/problems/dna/
+// 
+//        Version:  1.0
+//        Created:  28/Oct/2012 16:29:09
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Julio B. Silva (351202), julio(at)juliobs.com
+//        Company:  UFSCar
+// 
+// ============================================================================
+#include <cstdio>
+
+int main()
+{
+    int a, c, g, t;
+    char ch;
+
+    a = c = g = t = 0;
+    while ((ch=getchar()) != EOF)
+    {
+        switch(ch)
+        {
+            case 'A': a++; break;
+            case 'C': c++; break;
+            case 'G': g++; break;
+            case 'T': t++; break;
+        }
+    }
+
+    printf("%d %d %d %d\n", a, c, g, t);
+
+    return 0;
+}
---
+++ Rosalind/rna.cpp
@@ -0,0 +1,25 @@
+// ============================================================================
+// 
+//       Filename:  rna.cpp
+// 
+//    Description:  RNA Transcription
+//                  rosalind.info/problems/rna/
+// 
+//        Version:  1.0
+//        Created:  28/Oct/2012 16:53:02
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Julio B. Silva (351202), julio(at)juliobs.com
+//        Company:  UFSCar
+// 
+// ============================================================================
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
+
