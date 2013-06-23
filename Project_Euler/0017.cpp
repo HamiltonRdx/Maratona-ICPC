e15d5372e87ba09496f8a3ebb49ee7a9de444072
Julio Silva <julio@juliobs.com>
2013-06-23 16:24:15 -0300

    Project Euler 17 comentado

--- Project_Euler/0017.cpp
+++ Project_Euler/0017.cpp
@@ -18,12 +18,18 @@
 
 int main()
 {
+    // Números de 1 a 9. Contei as letras nos dedos
     int _1_9 = 36;
+    // Números de 10 a 19. Contei as letras nos dedos
     int _10_19 = 70;
+    // 8 dezenas 20s 30s 40s 50s 60s 70s 80s 90s aparecem 10 vezes cada
     int _20_99 = (6 + 6 + 5 + 5 + 5 + 7 + 6 + 6) * 10 + (_1_9 * 8);
+    // Soma de tudo calculado até agora
     int _1_99 = _1_9 + _10_19 + _20_99;
-    int _100_999 = (_1_9 + 7 * 9) + (_1_9 + 10 * 9) * 99 + _1_99 * 9;
-    int soma = _1_99 + _100_999 + 11;
+    int _100_999 = (_1_9 + 7 * 9) +       // hundred tem 7 letras e aparece 9x
+                   (_1_9 + 10 * 9) * 99 + // hundredand tem 10 letras
+                   _1_99 * 9;             // [1,99] aparece 1x em cada centena
+    int soma = _1_99 + _100_999 + 11;     // onethousand tem 11 letras
     printf("%d\n", soma);
     return 0;
 }
